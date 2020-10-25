90ed5a97dbbcfedb328a36dd63aafbc81edad7b7
matr0s <serhiy.osadchuk@gmail.com>
2020-10-25 22:17:03 +0200

    Get location method right after Widget creation

---
+++ scratch.dart
@@ -0,0 +1,31 @@
+import 'dart:async';
+
+void main() {
+  performTasks();
+}
+
+Future<void> performTasks() async {
+  task1();
+  String task2Result = await task2();
+  task3(task2Result);
+}
+
+void task1() {
+  String result = 'task 1 data';
+  print('Task 1 complete');
+}
+
+Future<String> task2() async {
+  Duration threeSeconds = Duration(seconds: 5);
+  String result;
+  await Future.delayed(threeSeconds, () {
+    result = 'task 2 data';
+    print('Task 2 comleted');
+  });
+  return result;
+}
+
+void task3(String taskTwoData) {
+  String result = 'task 3 data';
+  print('Task 3 complete with $taskTwoData');
+}
