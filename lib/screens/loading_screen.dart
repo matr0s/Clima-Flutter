cd1fcd5504e25e42e292173894398a858f4bea81
matr0s <serhiy.osadchuk@gmail.com>
2020-10-26 18:25:17 +0200

    Weather data added

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:clima/services/location.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -21,8 +23,25 @@ class _LoadingScreenState extends State<LoadingScreen> {
     print(location.longitude);
   }
 
+  void getWeatherData() async {
+    http.Response weatherResponse = await http.get(
+        'http://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&units=metric&appid=69060ed4df9937556570f4ec99d0ddff');
+    if (weatherResponse.statusCode == 200) {
+      String data = weatherResponse.body;
+
+      var decodedData = jsonDecode(data);
+
+      double weatherTemp = decodedData['main']['temp'];
+      int weatherType = decodedData['weather'][0]['id'];
+      String cityName = decodedData['name'];
+    } else {
+      print(weatherResponse.statusCode);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    getWeatherData();
     return Scaffold();
   }
 }
