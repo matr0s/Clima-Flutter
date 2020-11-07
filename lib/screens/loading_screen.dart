0d74d5b8e9f593ade5e984b5ba13b7a9086b1672
matr0s <serhiy.osadchuk@gmail.com>
2020-11-07 23:00:56 +0200

    Spinner added

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:clima/services/location.dart';
-import 'package:clima/services/networking.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+import '../services/location.dart';
+import '../services/networking.dart';
+
+import 'location_screen.dart';
 
 const kApiKey = '69060ed4df9937556570f4ec99d0ddff';
 
@@ -13,13 +17,14 @@ class _LoadingScreenState extends State<LoadingScreen> {
   double latitude;
   double longitude;
 
+  // Init State running every start of the App
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
     getLocationData();
   }
 
+  // Method we need for the first launch of the App
   void getLocationData() async {
     Location location = Location();
     await location.getCurrentLocation();
@@ -27,12 +32,24 @@ class _LoadingScreenState extends State<LoadingScreen> {
     longitude = location.longitude;
 
     NetworkHelper networkHelper = NetworkHelper(
-        'http://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&units=metric&appid=$kApiKey');
+        url:
+            'http://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&units=metric&appid=$kApiKey');
     var weatherData = await networkHelper.getData();
+    Navigator.push(context, MaterialPageRoute(builder: (context) {
+      return LocationScreen();
+    }));
   }
 
+  // Just empty Scaffold for the Loading Interface where we load initState
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: SpinKitDualRing(
+          color: Colors.white,
+          size: 100,
+        ),
+      ),
+    );
   }
 }
--- lib/services/networking.dart
+++ lib/services/networking.dart
@@ -1,16 +1,16 @@
+import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 class NetworkHelper {
-  NetworkHelper({this.url});
+  NetworkHelper({@required this.url});
 
   final String url;
 
-  Future<void> getData() async {
+  Future getData() async {
     http.Response weatherResponse = await http.get(url);
     if (weatherResponse.statusCode == 200) {
       String data = weatherResponse.body;
-
       return jsonDecode(data);
     } else {
       print(weatherResponse.statusCode);
