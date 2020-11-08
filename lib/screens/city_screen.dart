27facf62363113fc9ee9bd859d9dee7f3cbc87ee
matr0s <serhiy.osadchuk@gmail.com>
2020-11-08 14:32:35 +0200

    Error fixes

--- lib/screens/city_screen.dart
+++ lib/screens/city_screen.dart
@@ -1,4 +1,3 @@
-import 'package:clima/screens/location_screen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:clima/utilities/constants.dart';
